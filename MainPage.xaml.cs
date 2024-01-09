c6fea22a14c2714c1ab8b1a046c0e6af9894737d
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-01-09 09:13:05 +0100

    Simplify repro

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,22 +2,9 @@
 
 public partial class MainPage : ContentPage
 {
-    /// <summary>Command to initiate the start of a new script task run with the same parameters as the previous run.</summary>
-    public Command ClickCommand { get; }
-
     public MainPage()
 	{
-        ClickCommand = new(execute: () => {
-            DisplayAlert("Title", "Button 1 clicked", "Cancel");
-        });
-
         InitializeComponent();
-		BindingContext = this;
 	}
-
-    private void button2_Clicked(object sender, EventArgs e)
-    {
-        DisplayAlert("Title", "Button 2 clicked", "Cancel");
-    }
 }
 
